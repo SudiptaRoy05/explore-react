358c5afcfdbaf793ca197addfdc02e91fb5875d7
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-18 12:18:23 +0600

    handle btn

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,29 @@
 import './App.css'
 
 function App() {
+
+  function handleClick(){
+    alert('btn click')
+  }
+  const handleClicks=()=>{
+    alert('btn 2 clicked')
+  }
+
+
+  const addToFive=(num)=>{
+    alert(num+5);
+  }
+
   return (
     <>
       <h1>React Core Concepts</h1>
-      
-      
+      <button onClick={handleClick}>Click Me</button>
+      <hr />
+      <button onClick={handleClicks}>Click me 2</button>
+      <hr />
+      <button onClick={()=>{alert('third Clicked')}}>third</button>
+      <hr />
+      <button onClick={() =>addToFive(6)}>addToFive</button>
     </>
   )
 }
